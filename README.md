bbd74cda599477b82e02ddf7248cc8d9329e89ce
Mauricio <shinji.256@gmail.com>
2021-03-20 20:31:59 -0600

    Public repo for MAWMAWMAW

---
+++ README.md
@@ -0,0 +1,4 @@
+# Build of MAWMAWMAW website
+[See Website](https://mawmawmaw.github.io)
+[See Code Repo](https://github.com/mawmawmaw/mawmawmaw-dev)  
+![MAW](https://github.com/mawmawmaw/mawmawmaw.github.io/blob/master/images/logo192.jpg)
---
+++ sw.js
@@ -0,0 +1,118 @@
+var APP_PREFIX = 'MAWMAWMAW'
+var VERSION = '_v_01'
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [
+  '/',
+  '/images/logo192.png',
+  '/images/logo512.png',
+  '/images/fbtw.jpg',
+  '/images/radio.png',
+  '/sounds/music.mp3',
+  '/sounds/click.mp3',
+  '/sounds/sent.mp3',
+  '/sounds/typing.mp3',
+  '/sounds/poke.mp3',
+  '/sounds/lose.mp3',
+  '/static/css/10.5c1badd2.chunk.css',
+  '/static/css/10.5c1badd2.chunk.css.map',
+  '/static/css/11.48e940c9.chunk.css',
+  '/static/css/11.48e940c9.chunk.css.map',
+  '/static/css/4.2710a51e.chunk.css',
+  '/static/css/4.2710a51e.chunk.css.map',
+  '/static/css/6.e6f98312.chunk.css',
+  '/static/css/6.e6f98312.chunk.css.map',
+  '/static/css/7.f0ead6c2.chunk.css',
+  '/static/css/7.f0ead6c2.chunk.css.map',
+  '/static/css/8.db36526b.chunk.css',
+  '/static/css/8.db36526b.chunk.css.map',
+  '/static/css/9.661387e8.chunk.css',
+  '/static/css/9.661387e8.chunk.css.map',
+  '/static/css/main.dc06c83c.chunk.css',
+  '/static/css/main.dc06c83c.chunk.css.map',
+  '/static/js/10.b8a86ca2.chunk.js',
+  '/static/js/10.b8a86ca2.chunk.js.map',
+  '/static/js/11.562aad45.chunk.js',
+  '/static/js/11.562aad45.chunk.js.map',
+  '/static/js/2.29ae34cf.chunk.js',
+  '/static/js/2.29ae34cf.chunk.js.LICENSE.txt',
+  '/static/js/2.29ae34cf.chunk.js.map',
+  '/static/js/3.2671ee63.chunk.js',
+  '/static/js/3.2671ee63.chunk.js.LICENSE.txt',
+  '/static/js/3.2671ee63.chunk.js.map',
+  '/static/js/4.6213f17a.chunk.js',
+  '/static/js/4.6213f17a.chunk.js.map',
+  '/static/js/5.a2989d38.chunk.js',
+  '/static/js/5.a2989d38.chunk.js.map',
+  '/static/js/6.2f8bc32a.chunk.js',
+  '/static/js/6.2f8bc32a.chunk.js.map',
+  '/static/js/7.17e194b0.chunk.js',
+  '/static/js/7.17e194b0.chunk.js.map',
+  '/static/js/8.2ad6f026.chunk.js',
+  '/static/js/8.2ad6f026.chunk.js.map',
+  '/static/js/9.44da3dc3.chunk.js',
+  '/static/js/9.44da3dc3.chunk.js.map',
+  '/static/js/main.1ac6f5d3.chunk.js',
+  '/static/js/main.1ac6f5d3.chunk.js.map',
+  '/static/js/runtime-main.c5a8c501.js',
+  '/static/js/runtime-main.c5a8c501.js.map',
+  '/static/media/aws.abc17026.png',
+  '/static/media/bg_grass.bc75dcf9.png',
+  '/static/media/bg_sky.359b82b6.png',
+  '/static/media/bg_water.ccf8b3f5.png',
+  '/static/media/border.74df8f98.svg',
+  '/static/media/border_blue.83984a48.svg',
+  '/static/media/border_dark.7e52f862.svg',
+  '/static/media/border_red.11edf34b.svg',
+  '/static/media/game.d9d11b3d.ttf',
+  '/static/media/logo.91973642.png',
+  '/static/media/maw_poked.91d8a70a.gif',
+  '/static/media/maw_standing.b737e7eb.gif',
+  '/static/media/maw_walking.8ed401cf.gif',
+  '/static/media/maw_waving.c1f9aef7.gif',
+  '/static/media/mongodb.26af6c37.png',
+  '/static/media/php.41176e87.png',
+  '/static/media/postgresql.41f2547f.png',
+  '/static/media/react.bd6f202b.png',
+  '/static/media/redux.5c74c6db.png',
+  '/static/media/torn1.99ab1dbb.png',
+  '/static/media/torn2.01ddfff8.png',
+  '/static/media/webpack.97ad6798.png',
+  '/static/media/wordpress.b687aff4.png'
+]
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        return request
+      } else {
+        return fetch(e.request)
+      }
+    })
+  )
+})
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(URLS)
+    })
+  )
+})
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      cacheWhitelist.push(CACHE_NAME)
+
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
