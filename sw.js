8ea90060ff01395ca51320e973fd397b2c956b74
Mauricio <shinji.256@gmail.com>
2021-03-29 17:12:27 -0600

    Fixed: deleting old static files before push to repo

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ var APP_PREFIX = 'MAWMAWMAW'
 var VERSION = '_v_0_1b'
 var CACHE_NAME = APP_PREFIX + VERSION
 var URLS = [
-  '/index.html',
+  '/',
   '/images/logo192.jpg',
   '/images/logo512.jpg',
   '/images/fbtw.jpg',
@@ -27,8 +27,8 @@ var URLS = [
   '/static/css/8.b184a1d1.chunk.css.map',
   '/static/css/9.661387e8.chunk.css',
   '/static/css/9.661387e8.chunk.css.map',
-  '/static/css/main.8bbb8de8.chunk.css',
-  '/static/css/main.8bbb8de8.chunk.css.map',
+  '/static/css/main.78857880.chunk.css',
+  '/static/css/main.78857880.chunk.css.map',
   '/static/js/10.5f37d016.chunk.js',
   '/static/js/10.5f37d016.chunk.js.map',
   '/static/js/11.c9a6361d.chunk.js',
@@ -97,7 +97,9 @@ self.addEventListener('fetch', function (e) {
 self.addEventListener('install', function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
+      setTimeout(()=>{
         return cache.addAll(URLS)
+      },500)
     })
   )
 })
